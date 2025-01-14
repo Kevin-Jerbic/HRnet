4f4259ab44c062a62841151affc5f4e08230db7c
Jerbic Kevin <jerbic.kevin@outlook.fr>
2025-01-15 00:56:14 +0100

    Add data files

---
+++ src/data/departments.js
@@ -0,0 +1,25 @@
+const departments = [
+    {
+        label: 'Sales',
+        name: 'sales',
+    },
+    {
+        label: 'Marketing',
+        name: 'marketing',
+    },
+    {
+        label: 'Enginnering',
+        name: 'enginnering',
+    },
+    {
+        label: 'Human Ressources',
+        name: 'HR',
+    },
+
+    {
+        label: 'Legal',
+        name: 'legal',
+    },
+];
+
+export default departments;
